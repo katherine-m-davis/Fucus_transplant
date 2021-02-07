58ba82e21c4dbedd9462ad0b42dbefa5f67adaba
Katy Davis <kmdavis@biodiversity.ubc.ca>
2021-02-06 20:45:37 -0800

    Add rock PCoA

--- CG-FT_ReciprocalTransplant_PCoA_plots_Jan2021.R
+++ CG-FT_ReciprocalTransplant_PCoA_plots_Jan2021.R
@@ -71,15 +71,68 @@ p2 <- plot_ordination(ft.f, pcoa.ft.f, type = "samples", color="treatment", shap
   geom_point(size=3) +
   scale_shape_manual(values= ft.day.shapes) +
   scale_color_manual(values = ft.trmt.colors) +
-  labs(color='Treatment', shape = 'Sample day') 
+  labs(color='Treatment', shape = 'Sample day') +
+  labs(title = "F. distichus") +
+  theme(plot.title = element_text(face = "italic"))
 p2$layers <- p2$layers[-1]
 p2
 dev.off()
 
-# Save combined Figure 4 with labels
-pdf(file="~/Desktop/Desktop2020/Manuscripts/CG_FT_Manuscript_2020/CG_FT_Manuscript_Figures/CG-FT_FT_Figure4_PCoA_Jan2021.pdf",
-    width =6, height = 7)
-ggarrange(ftc, NULL, p2, ncol = 1, labels = c("A","","B"), heights=c(1,0.05,1))
+
+#### Rock Transplant by sample day and treatment, Figure 4B ####
+# Subset to only rock substrate samples
+ft.r <- subset_samples(ft, sample_data(ft)$type == "rock") 
+sample_data(ft.r)$sample.number <- as.character(sample_data(ft.r)$sample.number)
+
+# Ordinate
+pcoa.ft.r <- ordinate(ft.r, method = "PCoA", distance = "bray")
+
+# Day shapes and trmt colors
+ft.day.shapes <- c(16,1,2,6,5,8)
+ft.trmt.colors <- c("#E69F00","#463806", "#7ACE54", "darkblue")
+
+#### Plot and save Figure 4B ####
+pdf(file="~/Desktop/Desktop2020/Manuscripts/CG_FT_Manuscript_2020/CG_FT_Manuscript_Figures/CG-FT_ft_treatment_day_shapes_PCoA_Jan21.pdf",
+    width = 5, height = 3.5)
+p3 <- plot_ordination(ft.r, pcoa.ft.r, type = "samples", color="treatment", shape ="sample.number") +
+  theme_classic(base_size = 12) +
+  geom_point(size=3) +
+  scale_shape_manual(values= ft.day.shapes) +
+  scale_color_manual(values = ft.trmt.colors) +
+  labs(color='Treatment', shape = 'Sample day') +
+  labs(title = "Rock substrate") +
+  theme(legend.position = "none")
+p3$layers <- p3$layers[-1]
+p3
 dev.off()
 
 
+# Extract the legends. Returns a gtable
+legA <- get_legend(ftc)
+legB <- get_legend(p2)
+# Convert to a ggplot and print
+gg.A <- as_ggplot(legA) 
+gg.B <- as_ggplot(legB) 
+gg.B 
+
+
+gg.A <-gg.A + theme(plot.margin = unit(c(0, 0, 0, 0), "cm"))
+####################################################################################################################
+#### Combine plots with Figure A & B labels for Supplementary Figure ----------------------------------------------#
+library(cowplot)
+
+ftcl <- ftc + theme(legend.position = "none")
+p2l <- p2 + theme(legend.position = "none")
+p3l <- p3 + theme(legend.position = "none")
+
+
+# Save combined Figure 4 with labels
+pdf(file="~/Desktop/Desktop2020/Manuscripts/CG_FT_Manuscript_2020/CG_FT_Manuscript_Figures/CG-FT_FT_Figure4_PCoA_Feb2021.pdf",
+    width =7, height = 11)
+ggarrange(ggarrange(ftcl, NULL, p2l, NULL, p3l, ncol = 1, labels = c("A","","B", "", "C"), heights=c(1,0.05,1,0.05,1)),
+  ggarrange(gg.A, NULL, gg.B, NULL, NULL, ncol = 1, heights=c(1,0.25,1,0.05,0.7)), ncol = 2, widths=c(1, 0.5))
+dev.off()
+
+
+
+          
